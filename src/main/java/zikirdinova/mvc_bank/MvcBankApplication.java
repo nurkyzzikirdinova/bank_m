d415a4db2c3ec8eaee19e5253bebe931dbe3ae01
nurkyzzikirdinova <nzikirdinova7@gmail.com>
2024-08-13 15:14:35 +0600

    start

---
+++ src/main/java/zikirdinova/mvc_bank/MvcBankApplication.java
@@ -0,0 +1,17 @@
+package zikirdinova.mvc_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcBankApplication.class, args);
+       System.out.println("Start m_bank");
+    }
+
+
+
+
+}
---
+++ src/test/java/zikirdinova/mvc_bank/MvcBankApplicationTests.java
@@ -0,0 +1,13 @@
+package zikirdinova.mvc_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
